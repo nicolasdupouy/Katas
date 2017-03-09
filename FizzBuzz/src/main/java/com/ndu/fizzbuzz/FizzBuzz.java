f13741422c107f02879e2a9052a2fce7e5c91ce6
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2017-03-09 23:03:32 +0100

    Add first test and implementation.

---
+++ FizzBuzz/src/main/java/com/ndu/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package com.ndu.fizzbuzz;
+
+public class FizzBuzz {
+
+    public String display(int i) {
+        return "1";
+    }
+}
--- FizzBuzz/src/test/java/com/ndu/fizzbuzz/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/com/ndu/fizzbuzz/FizzBuzzTest.java
@@ -1,7 +1,21 @@
 package com.ndu.fizzbuzz;
 
-/**
- * Created by nicolas on 09/03/17.
- */
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void should_display_1_when_1() {
+        // Given
+        FizzBuzz fizzbuzz = new FizzBuzz();
+
+        // When
+        String result = fizzbuzz.display(1);
+
+        // Then
+        Assertions.assertEquals("1", result);
+    }
+
+
 }
