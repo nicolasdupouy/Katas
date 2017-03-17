6010f25741033b9e64017c1d5af6804f69f63f73
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2017-03-17 13:06:56 +0100

    Write basic code to pass multiple of 3 test

--- FizzBuzz/src/main/java/com/ndu/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/ndu/fizzbuzz/FizzBuzz.java
@@ -3,6 +3,9 @@ package com.ndu.fizzbuzz;
 public class FizzBuzz {
 
     public String display(int i) {
+        if (i%3 == 0) {
+            return "fizz";
+        }
         return String.valueOf(i);
     }
 }
