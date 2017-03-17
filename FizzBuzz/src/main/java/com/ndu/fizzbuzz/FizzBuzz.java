92b1543089019fad62c457318166f7f5b4f89cd5
Nicolas Dupouy (Debian SID @Edward) <nicolasdupouy@gmail.com>
2017-03-17 13:20:07 +0100

    Refactor after adding the buzz case test

--- FizzBuzz/src/main/java/com/ndu/fizzbuzz/FizzBuzz.java
+++ FizzBuzz/src/main/java/com/ndu/fizzbuzz/FizzBuzz.java
@@ -3,15 +3,20 @@ package com.ndu.fizzbuzz;
 public class FizzBuzz {
 
     public static final String FIZZ = "fizz";
+    public static final String BUZZ = "buzz";
 
     public String display(int number) {
         if (isMultipleOfThree(number)) {
             return FIZZ;
         }
-        if (number % 5 == 0) {
-            return "buzz";
+        if (ismultipleOfFive(number)) {
+            return BUZZ;
         }
         return String.valueOf(number);
+    }
+
+    private boolean ismultipleOfFive(int number) {
+        return number % 5 == 0;
     }
 
     private boolean isMultipleOfThree(int number) {
